7f3a9ec6dba1730cbd0dcceea8d0911c9dfe6e70
Elliot Richardson <ear78@aol.com>
2019-09-07 23:21:25 -0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+/*----------------------------------------
+ * sticky nav menu for sidebar on pages
+ -----------------------------------------*/
+require get_stylesheet_directory() . '/assets/vc_components/templates/sticky_nav_shortcode.php';
+vc_lean_map('sticky_nav_menu', null, get_stylesheet_directory() . '/assets/vc_components/mappings/sticky_nav_mapping.php');
+
+?>
